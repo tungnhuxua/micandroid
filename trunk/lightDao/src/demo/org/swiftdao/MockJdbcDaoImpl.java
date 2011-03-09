a55024db61b1cfb18a62fc28e83f254f1da41a41
zoopnin <zoopnin@29cfa68c-37ae-8048-bc30-ccda835e92b1>
2011-03-09 03:20:23 +0000

    zoopnin

---
+++ trunk/lightDao/src/demo/org/swiftdao/MockJdbcDaoImpl.java
@@ -0,0 +1,23 @@
+package org.swiftdao;
+
+import org.swiftdao.impl.JdbcDaoImpl;
+
+/**
+ * @author Wang Yuxing
+ *
+ */
+public class MockJdbcDaoImpl extends JdbcDaoImpl  implements MockJdbcDao {
+
+	public MockJdbcDaoImpl() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
